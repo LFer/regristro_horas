b1e8101edfdfed13e08de6e3a72c99cdbb61046f
Felipe <felipe@felipe-vm.(none)>
2015-11-13 23:22:40 -0300

    madurando...

--- models/registro.py
+++ models/registro.py
@@ -11,5 +11,28 @@ class registro(models.Model):
 
     check_in = fields.Datetime(string = 'Entrada', default = fields.Date.context_today)
     check_out = fields.Datetime(string = 'Salida')
+    detalle_ids = fields.One2many(comodel_name='detalle', inverse_name='registro_id')
+    #para probar las funciones
+    day = fields.Char(string="Dia")
+    hours = fields.Char(string="Horas")
+    left = fields.Char(string="Compensar")
+    overhour = fields.Char(string="Horas Extras")
+
+    @api.one
+    def dow(self):
+        pdb.set_trace()
+        date = self.check_in
+        days=["Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"]
+        dayNumber=date.weekday()
+        print days[dayNumber]    
 
 registro()
+
+class detalle(models.Model):
+    _name="detalle"
+
+    registro_id = fields.Many2one('registro')
+    day = fields.Char(string="Dia")
+    hours = fields.Char(string="Horas")
+    left = fields.Char(string="Compensar")
+    overhour = fields.Char(string="Horas Extras")
