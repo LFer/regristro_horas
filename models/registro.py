bc67703428dc721075422366f03e5a56c8c83c4e
Felipe Ferreira <lferreira@datamatic.com.uy>
2015-11-14 02:20:16 -0300

    Se escribe el dia de la entrada, se calculan las horas trabajadas en horas, deberia ser horas, minutos. Fix pendiente

--- models/registro.py
+++ models/registro.py
@@ -5,10 +5,15 @@ import logging
 import openerp.addons.decimal_precision as dp
 _logger = logging.getLogger(__name__)
 import ipdb as pdb
+import datetime
+import time
+from datetime import timedelta
+import math
+
 
 class registro(models.Model):
     _name="registro"
-
+#        
     check_in = fields.Datetime(string = 'Entrada', default = fields.Date.context_today)
     check_out = fields.Datetime(string = 'Salida')
     detalle_ids = fields.One2many(comodel_name='detalle', inverse_name='registro_id')
@@ -21,11 +26,44 @@ class registro(models.Model):
     @api.one
     def dow(self):
         pdb.set_trace()
+        var = str((datetime.date.today().strftime("%A"),datetime.date.today().strftime("%d"),"de",datetime.date.today().strftime("%B"),"del", datetime.date.today().strftime("%Y"))).replace("'","").replace(",","")
         date = self.check_in
         days=["Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"]
         dayNumber=date.weekday()
-        print days[dayNumber]    
 
+        print days[dayNumber]    
+    
+    @api.onchange('day')
+    def fecha(self):
+#        pdb.set_trace()
+        if not self.day:
+            var = str((datetime.date.today().strftime("%A"),datetime.date.today().strftime("%d"),"de",datetime.date.today().strftime("%B"),"del", datetime.date.today().strftime("%Y"))).replace("'","").replace(",","").replace("(","").replace(")","")
+            self.day = var
+        else:
+            print "No lo logro"
+
+#    @api.onchange('day')
+    @api.one
+    def horas_trabajadas(self):
+#        pdb.set_trace()        
+        res = ""
+        if (self.check_out and self.check_in) and (self.check_in <= self.check_out):
+            DATETIME_FORMAT = "%Y-%m-%d %H:%M:%S"
+            to_dt = datetime.datetime.strptime(self.check_out, DATETIME_FORMAT)
+            from_dt = datetime.datetime.strptime(self.check_in, DATETIME_FORMAT)
+            timedelta = to_dt - from_dt
+            diff_day = timedelta.days + float(timedelta.seconds) / 3600
+            res = diff_day
+            self.hours = res
+        return res
+#        horas divido entre 3600
+#        minutos divido entre 60
+            # puede servir
+            # c = time.strptime("2002-03-14 17:42:00","%Y-%m-%d %H:%M:%S")
+            # t = time.mktime(c)
+            # t = t + 1800 #30 minutes is 1800 secs
+            # time.strftime("%Y-%m-%d %H:%M:%S",time.localtime(t))
+            # '2002-03-14 18:12:00'
 registro()
 
 class detalle(models.Model):
