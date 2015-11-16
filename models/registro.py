ce6777c43adfe6d05d67df1d891dcedf8b1cbd47
LFer <lferreira@datamatic.com.uy>
2015-11-16 18:33:07 -0300

    Arreglar el onchange

--- models/registro.py
+++ models/registro.py
@@ -17,7 +17,7 @@ class registro(models.Model):
 #        
     check_in = fields.Datetime(string = 'Entrada', default = fields.Date.context_today)
     check_out = fields.Datetime(string = 'Salida')
-    detalle_ids = fields.One2many(comodel_name='detalle', inverse_name='registro_id')
+    detalle_ids = fields.One2many(comodel_name='detalle', inverse_name='registro_id')#, readonly=True)
     #para probar las funciones
     day = fields.Char(string="Dia")
     hours = fields.Char(string="Tiempo Trabajado")
@@ -26,25 +26,67 @@ class registro(models.Model):
     week_load = fields.Float(string="Carga Semanal")#, compute='_taken_seats')
 
 
+    @api.one
+    @api.onchange('detalle_ids')
+    def fecha(self):
+        lista = self.detalle_ids
+        if len(linsta)>5:
+            print "Hello"
 
-
+    @api.onchange('hours')
+    def fecha_this_will_work(self):
+        if self.hours:
+            pdb.set_trace()
+            pass
+        else:    
+            lista = self.detalle_ids
+            if len(linsta)>8:
+                print "Hello"
+    @api.one
     @api.onchange('day')
     def fecha(self):
 #        pdb.set_trace()
-
         if not self.day:
             var = str((datetime.date.today().strftime("%A"),datetime.date.today().strftime("%d"),"de",datetime.date.today().strftime("%B"),"del", datetime.date.today().strftime("%Y"))).replace("'","").replace(",","").replace("(","").replace(")","")
             self.day = var
         else:
             print "No lo logro"
+#        pdb.set_trace()
+   
 
-            
+
+
+
+
+        
+
+        # tabla = self.detalle_ids.ids     
+        # for x in tabla:
+        #     self.env.cr.execute(""" DELETE FROM ONLY detalle WHERE id = %(x)s  """,{'x':x})           
+    @api.one
+    def _check_lines(self):#TODO Hacerla mas escricta
+#        pdb.set_trace()
+        lista = self.detalle_ids.ids
+        if len(lista) !=0:    
+            for x in lista:
+                y = self.env['detalle'].browse(x)            
+                if y.check_in == self.check_in or y.check_out == self.check_out:
+                    raise exceptions.ValidationError("No puede Calcular horas para el mismo dia!")
+
+    # @api.one
+    # def _double_check(self,check_in, check_out):
+    #     lista = self.detalle_ids.ids
+    #     if len(lista) !=0:    
+    #         for x in lista:
+    #             y = self.env['detalle'].browse(x)            
+    #             if y.check_in == self.check_in or y.check_out == self.check_out:
+    #                 raise exceptions.ValidationError("No puede Calcular horas para el mismo dia!")       
+
 
     @api.one
     def horas_trabajadas(self):
-        tabla = self.detalle_ids.ids     
-        for x in tabla:
-            self.env.cr.execute(""" DELETE FROM ONLY detalle WHERE id = %(x)s  """,{'x':x})
+#        pdb.set_trace()
+        self._check_lines()
         res = ""
         if (self.check_out and self.check_in) and (self.check_in <= self.check_out):
             DATETIME_FORMAT = "%Y-%m-%d %H:%M:%S"
@@ -68,7 +110,7 @@ class registro(models.Model):
         if not self.check_out:
             raise exceptions.except_orm(_('Error!'), _('Debe Ingresar un Horario de Salida.'))
         if horas >= 9:
-            pdb.set_trace()
+#            pdb.set_trace()
             self.overhour = str(int(horas-9)) + ":" + minutostring
             self.left = ''
         else:
@@ -94,10 +136,11 @@ class detalle(models.Model):
     _name="detalle"
 
     registro_id = fields.Many2one('registro')
-    day = fields.Text(string="Dia")
-    hours = fields.Text(string="Horas")
-    left = fields.Text(string="Compensar")
-    overhour = fields.Text(string="Horas Extras")
+    day = fields.Char   (string="Dia")
+    hours = fields.Char(string="Horas")
+    left = fields.Char(string="Compensar")
+    overhour = fields.Char(string="Horas Extras")
     check_in = fields.Datetime(string = 'Entrada')
     check_out = fields.Datetime(string = 'Salida')    
     week_load = fields.Float(string="Carga Semanal")#, compute='_taken_seats')
+
