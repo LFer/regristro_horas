e52e84b3ec6b09ff2050af803dcf9e792010a8b2
Felipe Ferreira <lferreira@datamatic.com.uy>
2015-11-17 17:59:46 -0300

    Modulo funcionando

--- models/registro.py
+++ models/registro.py
@@ -14,8 +14,8 @@ import math
 class registro(models.Model):
     _name="registro"
     _rec_name="day"
-#        
-    check_in = fields.Datetime(string = 'Entrada', default = fields.Date.context_today)
+#
+    check_in = fields.Datetime(string = 'Entrada', default = datetime.date.today())
     check_out = fields.Datetime(string = 'Salida')
     detalle_ids = fields.One2many(comodel_name='detalle', inverse_name='registro_id')#, readonly=True)
     #para probar las funciones
@@ -23,129 +23,93 @@ class registro(models.Model):
     hours = fields.Char(string="Tiempo Trabajado")
     left = fields.Char(string="Compensar")
     overhour = fields.Char(string="Horas Extras")
-    week_load = fields.Float(string="Carga Semanal")#, compute='_taken_seats')
+    week_load = fields.Float(string="Carga Semanal")#, compute='_week_load')
+    var = fields.Float(string="test", default="3")
+
+
+
+    # @api.one
+    # @api.depends('week_load')
+    # def _week_load(self):
+    #     pdb.set_trace()
+    #     if not self.var:
+    #         self.week_load = 0.0
+    #     else:
+    #         self.week_load = 100.0 * self.hours / 45
+
+
+
+    def _function(self):
+        i = 0
+        if self.var<=36:
+            for x in (self.detalle_ids):
+                i += x.hours
+        self.var = i
 
 
     @api.one
-    @api.onchange('detalle_ids')
-    def fecha(self):
-        lista = self.detalle_ids
-        if len(linsta)>5:
-            print "Hello"
+    def _check_lenght_lines(self):
+#        pdb.set_trace()
+        if len(self.detalle_ids)>=5:
+            raise exceptions.ValidationError("No puede agregar mas de 5 linas por semana!")
 
-    @api.onchange('hours')
-    def fecha_this_will_work(self):
-        if self.hours:
-            #pdb.set_trace()
-            pass
-        else:    
-            lista = self.detalle_ids
-            if len(lista)>8:
-                print "Hello"
     @api.one
     @api.onchange('day')
     def fecha(self):
-        algo = self.day = str((datetime.date.today().strftime("%A"),datetime.date.today().strftime("%d"),"de",datetime.date.today().strftime("%B"),"del", datetime.date.today().strftime("%Y"))).replace("'","").replace(",","").replace("(","").replace(")","")
-        return algo 
-#        pdb.set_trace()
-        
-   
+        day = self.day = str((datetime.date.today().strftime("%A"),datetime.date.today().strftime("%d"),"de",datetime.date.today().strftime("%B"),"del", datetime.date.today().strftime("%Y"))).replace("'","").replace(",","").replace("(","").replace(")","")
+        return day
 
 
-
-
-
-        
-
-        # tabla = self.detalle_ids.ids     
-        # for x in tabla:
-        #     self.env.cr.execute(""" DELETE FROM ONLY detalle WHERE id = %(x)s  """,{'x':x})           
     @api.one
     def _check_lines(self):#TODO Hacerla mas escricta
 #        pdb.set_trace()
         lista = self.detalle_ids.ids
-        if len(lista) !=0:    
+        if len(lista) !=0:
             for x in lista:
-                y = self.env['detalle'].browse(x)            
+                y = self.env['detalle'].browse(x)
                 if y.check_in == self.check_in or y.check_out == self.check_out:
                     raise exceptions.ValidationError("No puede Calcular horas para el mismo dia!")
-
-    # @api.one
-    # def _double_check(self,check_in, check_out):
-    #     lista = self.detalle_ids.ids
-    #     if len(lista) !=0:    
-    #         for x in lista:
-    #             y = self.env['detalle'].browse(x)            
-    #             if y.check_in == self.check_in or y.check_out == self.check_out:
-    #                 raise exceptions.ValidationError("No puede Calcular horas para el mismo dia!")       
 
 
     @api.one
     def horas_trabajadas(self):
-        pdb.set_trace()
+#        pdb.set_trace()
         self._check_lines()
+        self._check_lenght_lines()
         res = ""
         if (self.check_out and self.check_in) and (self.check_in <= self.check_out):
             DATETIME_FORMAT = "%Y-%m-%d %H:%M:%S"
             to_dt = datetime.datetime.strptime(self.check_out, DATETIME_FORMAT)
             from_dt = datetime.datetime.strptime(self.check_in, DATETIME_FORMAT)
-            timedelta = to_dt - from_dt
-            diff_day = float(timedelta.seconds) / 3600
-            res = diff_day
-#            self.hours = res
+            tiempo_trabajado = to_dt - from_dt
+            self.hours = tiempo_trabajado
+            tope = datetime.timedelta(0, 32400)
+            if tiempo_trabajado.seconds == tope.seconds:
+                self.left = ''
+                self.overhour = ''
+                self.week_load = 100 * tiempo_trabajado.seconds / tope.seconds
+            if tiempo_trabajado.seconds >= tope.seconds:
+                if tiempo_trabajado.seconds - tope.seconds > 0:
+                    self.overhour = tiempo_trabajado - tope
+                    self.left = ''
+                    self.week_load = 100 * tiempo_trabajado.seconds / tope.seconds
+            else:
+                self.left = tope - tiempo_trabajado
+                self.week_load = 100 * tiempo_trabajado.seconds / tope.seconds
+                self.overhour = ''
 
-            horas = round(math.floor(diff_day))
-            if horas.is_integer():
-                minuts = diff_day - int(diff_day)
-                minuts2 = minuts*60
-                miutosletra = str(minuts2).replace('.','').replace('0','')
-                horaletra = str(horas).replace('.',':') #.replace('0','00:')
-                self.hours = horaletra + miutosletra
-#                self.overhour = 
-    
-    #            horas = diff_day - int(diff_day)
-                horastring = str(horas).replace('.',':').replace('0','')
-            if res - int(res) == 0:
-                self.hours = horastring + "00"
-#            else:
-                x = res - int(res)
-                minutos = x*60
-                minutosfloor = round(math.floor(minutos))
-                segundos = minutos/60
-                segundosfloor = str(segundos - int(segundos))[1:4].replace('.',':')
-                #para devolver pasamos todo a strings
-                minutostring = str(minutosfloor).replace('0','').replace('.','')
-                horas_trabajadas = horastring + minutostring
-                self.hours = horas_trabajadas
-                if not self.check_out:
-                    raise exceptions.except_orm(_('Error!'), _('Debe Ingresar un Horario de Salida.'))
-                if horas >= 9:
-    #                if res - int(res) == 0:
-        #            pdb.set_trace()
-                    x = res - int(res)
-                    minutos = x*60
-                    minutosfloor = round(math.floor(minutos))
-                    minutostring = str(minutosfloor).replace('0','').replace('.','')
-                    if minutostring == '':
-                        self.overhour = ''
-                    else:
-                        self.overhour = str(int(horas-9)) + ":" + minutostring + ""
-                        self.left = ''
-                else:
-                    self.left = str(int(math.fabs(int(horas-9)))) + ":" + str(int(60-minutos))
-                    self.overhour = ''
-                    
-                lineas = {
-                        'day':self.day,
-                        'hours':self.hours,
-                        'left':self.left,
-                        'overhour':self.overhour,
-                        'check_in':self.check_in,
-                        'check_out':self.check_out,
-                        }
-                lineas2=[(0,0,lineas)]
-                return self.write({'detalle_ids':lineas2})
-                
+            lineas = {
+                    'day':self.day,
+                    'hours':self.hours,
+                    'left':self.left,
+                    'overhour':self.overhour,
+                    'check_in':self.check_in,
+                    'check_out':self.check_out,
+                    'week_load':self.week_load,
+                    }
+            lineas2=[(0,0,lineas)]
+            return self.write({'detalle_ids':lineas2})
+
 
 
 registro()
@@ -159,6 +123,13 @@ class detalle(models.Model):
     left = fields.Char(string="Compensar")
     overhour = fields.Char(string="Horas Extras")
     check_in = fields.Datetime(string = 'Entrada')
-    check_out = fields.Datetime(string = 'Salida')    
-    week_load = fields.Float(string="Carga Semanal")#, compute='_taken_seats')
+    check_out = fields.Datetime(string = 'Salida')
+    week_load = fields.Float(string="Carga Semanal")#, compute='_carga_semanal')
 
+    # @api.one
+    # @api.depends('hours', 'week_load')
+    # def _carga_semanal(self):
+    #     if not self.hours:
+    #         self.week_load = 0.0
+    #     else:
+    #         self.week_load = 100 * self.hours/9
