fd77407fb28f448f453293c12a3b8107320565ae
Felipe Ferreira <lferreira@datamatic.com.uy>
2015-11-24 10:05:23 -0300

    Estimando hora de salida correctamente

--- models/registro.py
+++ models/registro.py
@@ -9,12 +9,14 @@ import datetime
 import time
 from datetime import timedelta
 import math
+from datetime import date
 
 
 class registro(models.Model):
     _name="registro"
     _rec_name="day"
 #
+    name = fields.Char()
     check_in = fields.Datetime(string = 'Entrada', default =datetime.date.today())
     check_out = fields.Datetime(string = 'Salida')
     detalle_ids = fields.One2many(comodel_name='detalle', inverse_name='registro_id')#, readonly=True)
@@ -46,7 +48,7 @@ class registro(models.Model):
 
     @api.one
     def _check_lenght_lines(self):
-#        
+#
         if len(self.detalle_ids)>=5:
             raise exceptions.ValidationError("No puede agregar mas de 5 linas por semana!")
     def _function(self):
@@ -59,34 +61,34 @@ class registro(models.Model):
 
     @api.one
     def _check_lenght_lines(self):
-#        
+#
         if len(self.detalle_ids)>=5:
             raise exceptions.ValidationError("No puede agregar mas de 5 linas por semana!")
 
-    @api.one
-    @api.onchange('check_in')
-    def fecha_entrada(self):
-#        pdb.set_trace()
-        day = str((datetime.date.today().strftime("%A"),datetime.date.today().strftime("%d"),"de",datetime.date.today().strftime("%B"),"del", datetime.date.today().strftime("%Y"))).replace("'","").replace(",","").replace("(","").replace(")","")
-        if not self.check_in:
-            pass
-        else:
-            self.day = day
+#     @api.one
+#     @api.onchange('check_in')
+#     def fecha_entrada(self):
+# #        pdb.set_trace()
+#         day = str((datetime.date.today().strftime("%A"),datetime.date.today().strftime("%d"),"de",datetime.date.today().strftime("%B"),"del", datetime.date.today().strftime("%Y"))).replace("'","").replace(",","").replace("(","").replace(")","")
+#         if not self.check_in:
+#             pass
+#         else:
+#             self.day = day
 
-    @api.one
-    @api.onchange('check_in')
-    def fecha(self):
-#        pdb.set_trace()
-        day = str((datetime.date.today().strftime("%A"),datetime.date.today().strftime("%d"),"de",datetime.date.today().strftime("%B"),"del", datetime.date.today().strftime("%Y"))).replace("'","").replace(",","").replace("(","").replace(")","")
-        if not self.day:
-            self.day = day
-        else:
-            self.day = day
-        
-        if self.day:
-            self.day = day
-        else:
-            self.day = day            
+#     @api.one
+#     @api.onchange('check_in')
+#     def fecha(self):
+# #        pdb.set_trace()
+#         day = str((datetime.date.today().strftime("%A"),datetime.date.today().strftime("%d"),"de",datetime.date.today().strftime("%B"),"del", datetime.date.today().strftime("%Y"))).replace("'","").replace(",","").replace("(","").replace(")","")
+#         if not self.day:
+#             self.day = day
+#         else:
+#             self.day = day
+
+#         if self.day:
+#             self.day = day
+#         else:
+#             self.day = day
 
     @api.one
     def _check_lines(self):#TODO Hacerla mas escricta
@@ -97,6 +99,46 @@ class registro(models.Model):
                 y = self.env['detalle'].browse(x)
                 if y.check_in == self.check_in or y.check_out == self.check_out:
                     raise exceptions.ValidationError("No puede Calcular horas para el mismo dia!")
+
+    @api.one
+    def estimar_horas_trabajadas(self):
+#        pdb.set_trace()
+        res = ""
+        if self.check_in:
+            DATETIME_FORMAT = "%Y-%m-%d %H:%M:%S"
+            DATETIME_FORMATEO = "%H:%M:%S"
+            from_dt = datetime.datetime.strptime(self.check_in, DATETIME_FORMAT)
+            tope = datetime.timedelta(0, 32400)
+            tope2 = datetime.timedelta(0,21600)
+            hoy = datetime.datetime.now()
+            ahora = date
+            string_hoy = str(hoy)
+            corte = string_hoy[:19]
+            hoy_objeto = datetime.datetime.strptime(corte, DATETIME_FORMAT)
+            #nos quedamos solo con las horas
+            #paso a sintg y me quedo con la hora
+            entrada = str(from_dt)[11:]
+            #lo paso a objecto datetime
+            entrada_objeto = datetime.datetime.strptime(entrada, DATETIME_FORMATEO)
+            salida_aproximada = from_dt + tope2
+            salida_string = str(salida_aproximada)[11:]
+            self.hora_aproximada = salida_string
+
+    @api.one
+    def sumer_horas_semanales(self):
+        pdb.set_trace()
+        variales = ""
+        algunas = ()
+        dejare = []
+        para_ver = {}
+        DATETIME_FORMAT = "%H:%M:%S"
+
+        x = self.detalle_ids
+        lunes = datetime.datetime.strptime(x[0].hours, DATETIME_FORMAT)
+        martes = datetime.datetime.strptime(x[1].hours, DATETIME_FORMAT)
+        miercoles = datetime.datetime.strptime(x[2].hours, DATETIME_FORMAT)
+        jueves = datetime.datetime.strptime(x[3].hours, DATETIME_FORMAT)
+        viernes = datetime.datetime.strptime(x[4].hours, DATETIME_FORMAT)
 
 
     @api.one
@@ -147,8 +189,9 @@ registro()
 class detalle(models.Model):
     _name="detalle"
 
+    name = fields.Char()
     registro_id = fields.Many2one('registro')
-    day = fields.Char(string="Dia", compute='_get_day')
+    day = fields.Char(string="Dia")#, compute='_get_day')
     hours = fields.Char(string="Horas")
     left = fields.Char(string="Compensar")
     overhour = fields.Char(string="Horas Extras")
@@ -164,8 +207,8 @@ class detalle(models.Model):
     #     else:
     #         self.week_load = 100 * self.hours/9
 
-    @api.one
-    @api.depends('day')
-    def _get_day(self):
-        day = str((datetime.date.today().strftime("%A"),datetime.date.today().strftime("%d"),"de",datetime.date.today().strftime("%B"),"del", datetime.date.today().strftime("%Y"))).replace("'","").replace(",","").replace("(","").replace(")","")
-        self.day = day
+    # @api.one
+    # @api.depends('day')
+    # def _get_day(self):
+    #     day = str((datetime.date.today().strftime("%A"),datetime.date.today().strftime("%d"),"de",datetime.date.today().strftime("%B"),"del", datetime.date.today().strftime("%Y"))).replace("'","").replace(",","").replace("(","").replace(")","")
+    #     self.day = day
